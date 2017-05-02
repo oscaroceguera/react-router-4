2009b10a57a0e5344db29e4c0e32325d050b494f
oscaroceguera <oscaroceguera@gmail.com>
2017-05-02 11:56:35 -0500

    Custom Link

--- src/App.js
+++ src/App.js
@@ -2,102 +2,40 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link,
-  Redirect,
-  withRouter
+  Link
 } from 'react-router-dom'
 
-////////////////////////////////////////////////////////////
-// 1. Click the public page
-// 2. Click the protected page
-// 3. Log in
-// 4. Click the back button, note the URL each time
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
 
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
 
-
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    this.isAuthenticated = true
-    setTimeout(cb, 100) // fake async
-  },
-  signout(cb) {
-    this.isAuthenticated = false
-    setTimeout(cb, 100)
-  }
-}
-
-const AuthButton = withRouter(({ history }) => (
-  fakeAuth.isAuthenticated ? (
-    <p>
-      Welcome ! <button onClick={() => {
-        fakeAuth.signout(() => history.push('/'))
-      }}></button>
-    </p>
-  ) : (
-    <p>You are not logged in.</p>
-  )
-))
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={props => (
-    fakeAuth.isAuthenticated ? (
-      <Component {...props} />
-    ) : (
-      <Redirect to={{
-        pathname:'/login',
-        state: { from: props.location }
-      }}/>
-    )
+const OldScoolMenuLink = ({ label, to, activeOnlyWhenExact }) => (
+  <Route path={to} exact={activeOnlyWhenExact} children={({match}) => (
+    <div className={match ? 'active' : ''}>
+      {match ? '>' : ''}<Link to={to}>{label}</Link>
+    </div>
   )} />
 )
 
-const Public = () => <h3>Public</h3>
-const Protected = () => <h3>Protected</h3>
-
-class Login extends React.Component {
-  state = {
-    redirectToReferrer: false
-  }
-
-  login = () => {
-    fakeAuth.authenticate(() => {
-      this.setState({ redirectToReferrer: true })
-    })
-  }
-
-  render() {
-    const { from } = this.props.location.state || { from: { pathname : '/' }}
-    const { redirectToReferrer } = this.state
-
-    if (redirectToReferrer) {
-      return (
-        <Redirect to={from} />
-      )
-    }
-
-    return (
-      <div>
-        <p>You must log in to view the page at { from.pathname }</p>
-        <button onClick={this.login}>Log in</button>
-      </div>
-    )
-  }
-}
-
-const AuthExample = () => (
+const CustomLinkExample = () => (
   <Router>
     <div>
-      <AuthButton />
-      <ul>
-        <li><Link to="/public">Public Page</Link></li>
-        <li><Link to="/protected">Protected Page</Link></li>
-      </ul>
-      <Route path="/public" component={Public} />
-      <Route path="/login" component={Login} />
-      <PrivateRoute path="/protected" component={Protected} />
+      <OldScoolMenuLink activeOnlyWhenExact={true} to="/" label="Home" />
+      <OldScoolMenuLink to='/about' label='About' />
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
   </Router>
 )
 
-export default AuthExample
+export default CustomLinkExample
