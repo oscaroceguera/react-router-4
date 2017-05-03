ba57dd3d02e3c6e4196de1d1fa853aa4df4047f5
oscaroceguera <oscaroceguera@gmail.com>
2017-05-03 10:46:38 -0500

    Preventing Transitions

--- src/App.js
+++ src/App.js
@@ -2,40 +2,72 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Prompt
 } from 'react-router-dom'
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
+class Form extends React.Component {
+  state = {
+    isBlocking: false
+  }
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
+  render () {
+    const { isBlocking } = this.state
 
-const OldScoolMenuLink = ({ label, to, activeOnlyWhenExact }) => (
-  <Route path={to} exact={activeOnlyWhenExact} children={({match}) => (
-    <div className={match ? 'active' : ''}>
-      {match ? '>' : ''}<Link to={to}>{label}</Link>
-    </div>
-  )} />
-)
+    return (
+      <form
+        onSubmit={event => {
+          event.preventDefault()
+          event.target.reset()
+          this.setState({
+            isBlocking: false
+          })
+        }}>
 
-const CustomLinkExample = () => (
+        <Prompt
+          when={isBlocking}
+          message={location => (
+            `Are you sure you want to go to ${location.pathname}`
+          )}
+        />
+
+        <p>
+          Blocking? {isBlocking ? 'yes, click a link or the back button' : 'Nope'}
+        </p>
+
+        <p>
+          <input
+            size="50"
+            placeholder="type something to lock transitions"
+            onChange={event => {
+              this.setState({
+                isBlocking: event.target.value.length > 0
+              })
+            }}
+          />
+        </p>
+
+        <p>
+          <button>Submit to stop blocking</button>
+        </p>
+      </form>
+    )
+  }
+}
+
+const PreventingTransitionExample = () => (
   <Router>
     <div>
-      <OldScoolMenuLink activeOnlyWhenExact={true} to="/" label="Home" />
-      <OldScoolMenuLink to='/about' label='About' />
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
+      <ul>
+        <li><Link to="/">Form</Link></li>
+        <li><Link to="/one">One</Link></li>
+        <li><Link to="/two">Two</Link></li>
+      </ul>
+      <Route path="/" exact component={Form} />
+      <Route path="/one" render={() => <h3>One</h3>}/>
+      <Route path="/two" render={() => <h3>two</h3>}/>
     </div>
   </Router>
 )
 
-export default CustomLinkExample
+export default PreventingTransitionExample
