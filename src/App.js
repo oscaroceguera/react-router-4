36fb6f90b42a77d1f92b4a036ab8b6434704926c
oscaroceguera <oscaroceguera@gmail.com>
2017-05-05 12:04:13 -0500

    Animated Transition

--- src/App.js
+++ src/App.js
@@ -1,67 +1,98 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Redirect
 } from 'react-router-dom'
 
-const routes = [
-  { path: '/',
-    exact: true,
-    sidebar: () => <div>home!</div>,
-    main: () => <h2>Home</h2>
-  },
-  { path: '/bubblegum',
-    sidebar: () => <div>bubblegum!</div>,
-    main: () => <h2>Bubblegum</h2>
-  },
-  { path: '/shoelaces',
-    sidebar: () => <div>shoelaces!</div>,
-    main: () => <h2>Shoelaces</h2>
-  }
-]
+const styles = {}
 
-const SidebarExample = () => (
+styles.fill = {
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0
+}
+
+styles.content = {
+  ...styles.fill,
+  top: '40px',
+  textAlign: 'center'
+}
+
+styles.nav = {
+  padding: 0,
+  margin: 0,
+  position: 'absolute',
+  top: 0,
+  height: '40px',
+  width: '100%',
+  display: 'flex'
+}
+
+styles.navItem = {
+  textAlign: 'center',
+  flex: 1,
+  listStyleType: 'none',
+  padding: '10px'
+}
+
+styles.hsl  = {
+  ...styles.fill,
+  color: 'white',
+  paddingTop: '20px',
+  fontSize: '30px'
+}
+
+const NavLink = (props) => (
+  <li style={styles.navItem}>
+    <Link {...props} style={{ color: 'inherit' }} />
+  </li>
+)
+
+const HSL = ({ match: { params } }) => (
+  <div style={{
+    ...styles.fill,
+    ...styles.hsl,
+    background: `hsl(${params.h}, ${params.s}%, ${params.l}%)`
+  }}>
+    hsl({params.h}, {params.s}%, {params.l}%)
+  </div>
+)
+
+const AnimationExample = () => (
   <Router>
-    <div style={{ display: 'flex' }}>
-      <div style={{
-        padding: '10px',
-        width: '40%',
-        background: '#f0f0f0'
-      }}>
-        <ul style={{ listStyleType: 'none', padding: 0 }}>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/bubblegum">Bubblegum</Link></li>
-          <li><Link to="/shoelaces">Shoelaces</Link></li>
-        </ul>
+    <div style={styles.fill}>
+      <Route render={({ location }) => (
+        <Redirect to="/10/90/50" />
+      )}/>
 
-        {
-          routes.map(({path, exact, sidebar}, index) => (
-            <Route
-              key={index}
-              path={path}
-              exact={exact}
-              component={sidebar}
-            />
-          ))
-        }
+      <ul style={styles.nav}>
+        <NavLink to="/10/90/50">Red</NavLink>
+        <NavLink to="/120/100/40">Green</NavLink>
+        <NavLink to="/200/100/40">Blue</NavLink>
+        <NavLink to="/310/100/50">Pink</NavLink>
+      </ul>
 
-        <div style={{ flex: 1, padding: '10px' }}>
-          {
-            routes.map(({path, exact, main}, index) => (
-              <Route
-                key={index}
-                path={path}
-                exact={exact}
-                component={main}
-              />
-            ))
-          }
-        </div>
+      <div style={styles.content}>
+        <ReactCSSTransitionGroup
+          transitionName="fade"
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}
+        >
+          <Route
+            location={location}
+            key={location.key}
+            path="/:h/:s/:l"
+            component={HSL}
+          />
+        </ReactCSSTransitionGroup>
       </div>
-
     </div>
   </Router>
 )
 
-export default SidebarExample
+export default AnimationExample
